6caa3f9b274fba0bae625b30334c283e081d2b4e
Timothy <tdfrese@gmail.com>
2018-04-03 17:29:31 -0500

    More LanternMovement states implemented

--- LanternGhost/Assets/LanternMovement.cs
+++ LanternGhost/Assets/LanternMovement.cs
@@ -14,8 +14,9 @@ public class LanternMovement : MonoBehaviour {
     private Vector2 throwTarget;
 	public Rigidbody2D humanBody;
 	public Rigidbody2D lanternBody;
-    private float throwSpeed;
+	private float throwTime;
     private const float WIND_UP_MULTIPLIER = 1f;
+	private float t = 0;
 
     void Start () {
         lantern = GetComponent<Lantern>();
@@ -23,7 +24,7 @@ public class LanternMovement : MonoBehaviour {
         //humanBody = lantern.players.human.GetComponent<Rigidbody2D>();
 		humanBody = lantern.transform.parent.GetComponentInChildren<Human>().GetComponent<Rigidbody2D>();
         lanternBody = lantern.GetComponent<Rigidbody2D>();
-        throwSpeed = 5;
+        throwTime = 2;
     }
 	
 	void FixedUpdate () {
@@ -40,43 +41,59 @@ public class LanternMovement : MonoBehaviour {
          */
 
 		bool throwButtonDown = Input.GetButton ("Throw");
-        switch (lantern.lanternState)
-        {
-            case Lantern.LanternState.HELD:
-                //did we click?
-                if (throwButtonDown)
-                {
-                    lantern.lanternState = Lantern.LanternState.WINDING_UP;
-                }
-                break;
-            case Lantern.LanternState.WINDING_UP:
-                if (throwButtonDown)
-                {
-                    windUpTime += Time.fixedDeltaTime;
-                }
-                else
-                {
-                    lantern.lanternState = Lantern.LanternState.THROWING;
-                    throwTarget = (lantern.players.human.faceDirection * WIND_UP_MULTIPLIER * windUpTime) + humanBody.position;
-					windUpTime = 0;
-                }
-                break;
-            case Lantern.LanternState.THROWING:
-                lanternBody.position = Vector2.Lerp(lanternBody.position, throwTarget, throwSpeed * Time.fixedDeltaTime);
-                break;
-            case Lantern.LanternState.RECALLING:
-                break;
-            case Lantern.LanternState.SEPARATED:
-                break;
-            default:
-                throw new System.Exception("Missing lantern state enum option!");
+		Vector2 newPosition;
+        switch (lantern.lanternState) {
+		case Lantern.LanternState.HELD:
+			lanternBody.position = humanBody.position;
+	        if (throwButtonDown)
+	        {
+	            lantern.lanternState = Lantern.LanternState.WINDING_UP;
+	        }
+	        break;
+        case Lantern.LanternState.WINDING_UP:
+			lanternBody.position = humanBody.position;
+            if (throwButtonDown) {
+                windUpTime += Time.fixedDeltaTime;
+            }
+            else {
+                lantern.lanternState = Lantern.LanternState.THROWING;
+                throwTarget = (lantern.players.human.faceDirection * WIND_UP_MULTIPLIER * windUpTime) + humanBody.position;
+				windUpTime = 0;
+            }
+            break;
+		case Lantern.LanternState.THROWING:
+			t += Time.fixedDeltaTime / throwTime;
+			newPosition = Vector2.Lerp (lanternBody.position, throwTarget, t);
+			if (lanternBody.position == newPosition) {
+				lantern.lanternState = Lantern.LanternState.SEPARATED;
+				t = 0;
+			} else {
+				lanternBody.position = newPosition;
+			}
+            break;
+		case Lantern.LanternState.SEPARATED:
+			if (throwButtonDown) {
+				lantern.lanternState = Lantern.LanternState.RECALLING;
+			}
+			break;
+        case Lantern.LanternState.RECALLING:
+			t += Time.fixedDeltaTime / throwTime;
+			newPosition = Vector2.Lerp (lanternBody.position, humanBody.position, t);
+			if (lanternBody.position == newPosition) {
+				lantern.lanternState = Lantern.LanternState.HELD;
+				t = 0;
+			} else {
+				lanternBody.position = newPosition;
+			}
+            break;
+        default:
+            throw new System.Exception("Missing lantern state enum option!");
         }
 
         Foo();
     }
 
-    private void Foo ()
-    {
+    private void Foo () {
 
     }
 }
